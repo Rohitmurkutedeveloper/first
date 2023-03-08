d57ff52188ac14e2eee862d2e258cfdaafb8e49f
Rohitmurkutedeveloper <rohitmurkute20@gmail.com>
2023-03-08 12:50:00 +0530

    This is a new commit for what I originally planned to be amended

---
+++ firstscript.js
@@ -0,0 +1,10 @@
+		
+		
+		
+		    a=document.getElementsByClassName("a");
+			   b=document.getElementsByClassName("b");
+		      document.getElementById("h").innerHTML="hello";
+		
+				a[0].innerHTML="OK";
+				b[0].innerHTML="THIK HAI";
+				
---
+++ query.js
@@ -0,0 +1,20 @@
+<html>
+<body>
+<h1>this is jquery</h1>
+<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+
+<script>
+
+
+$(document).write("hello");
+$("h1").click(function(){
+
+alert("h1 clicked");
+});
+
+
+
+
+
+</body>
+</html>
